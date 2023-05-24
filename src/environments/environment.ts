3543805b42cf5db94aee37c5721e0ddd98dee836
WrappedUsername <wrappedusername@gmail.com>
2023-05-24 15:09:15 -0600

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  firebase: {
+    projectId: 'picklemonstercinematics',
+    appId: '1:981631940666:web:46e100862b7271ba629d03',
+    storageBucket: 'picklemonstercinematics.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDKCYkWBaeZkSakCjavA4xJ5cHHdYWfTU8',
+    authDomain: 'picklemonstercinematics.firebaseapp.com',
+    messagingSenderId: '981631940666',
+    measurementId: 'G-RPRKQ4ESRB',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDKCYkWBaeZkSakCjavA4xJ5cHHdYWfTU8",
+    authDomain: "picklemonstercinematics.firebaseapp.com",
+    projectId: "picklemonstercinematics",
+    storageBucket: "picklemonstercinematics.appspot.com",
+    messagingSenderId: "981631940666",
+    appId: "1:981631940666:web:46e100862b7271ba629d03",
+    measurementId: "G-RPRKQ4ESRB",
+  },
+};
